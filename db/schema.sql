d095a276e1c08d1994aa4fa18bf4c3b9aa1ade56
Brooks Burwell <brooksdburwell@gmail.com>
2019-07-15 17:02:21 -0700

    pushing to heroku

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    burger_name VARCHAR(60) NOT NULL,
+    devoured BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Vegan Burger", false);
